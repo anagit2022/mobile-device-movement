11e0df3a160d474540777ec0ddadee87f223d621
anagit2022 <107537622+anagit2022@users.noreply.github.com>
2025-09-29 08:57:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,22 +18,11 @@ function setup() {
 function draw() {
   if (!permissionGranted) return;
 
-  background(0, 64);
-  if (frameCount % 30 === 0) {  // update once every 30 frames (~0.5 sec at 60fps)
-    ax = accelerationX;
-    ay = accelerationY;
-    az = accelerationZ;
-  }
-
-  text("X: " + nf(ax, 1, 2), 20, 40);
-  text("Y: " + nf(ay, 1, 2), 20, 80);
-  text("Z: " + nf(az, 1, 2), 20, 120);
-}
-  let x = map(accelerationX, -10, 10, 0, width);
-  let y = map(accelerationY, -10, 10, 0, height);
-  let diameter = map(accelerationZ, -10, 10, 10, 100);
-
-  circle(x, y, diameter);
+ 
+   background(220, 50);
+  fill('magenta');
+  ellipse(width / 2, height / 2, accelerationX);
+  describe('Magnitude of device acceleration is displayed as ellipse size.');
 }
 
 function requestAccess() {
